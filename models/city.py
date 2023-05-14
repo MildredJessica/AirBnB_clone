19c00bc3e91df034b9baa4b952ae9352c4fdf373
EnobongEdo <onlynenny@gmail.com>
2023-05-14 13:40:11 -0700

    task 9

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is a module for city class"""
+
+from base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+        city class
+    """
+    state_id = ""
+    name = ""
