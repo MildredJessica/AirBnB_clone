1826003a761c250bab04f1a4958408a9505fb52d
EnobongEdo <onlynenny@gmail.com>
2023-05-14 13:29:03 -0700

    task 9

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This is a module for state class"""
+
+from base_model import BaseModel
+
+
+class State(BaseModel):
+    """class represents a state."""
+    
+    name = ""
