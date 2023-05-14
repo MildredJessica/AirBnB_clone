7c40de15118a210e4929d8ed427f9e3cdbb26e1f
EnobongEdo <onlynenny@gmail.com>
2023-05-14 14:21:27 -0700

    task 9

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module is for User class."""
+from base_model import BaseModel
+
+class User(BaseModel):
+    """Class represents a User."""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
