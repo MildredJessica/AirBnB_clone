831b60dcec18af288d0311eda7ed11aa998ebbcb
MildredJessica <nwonyejessica@gmail.com>
2023-05-16 22:28:41 +0100

    Removed the the introduction

--- console.py
+++ console.py
@@ -7,16 +7,18 @@ class HBNBCommand(cmd.Cmd):
     """
         Command line for the HBNB shell
     """
-    intro = 'Welcome to the HBNB shell.   Type help or ? to list commands.\n'
+    # intro = 'Welcome to the HBNB shell.   Type help or ? to list commands.\n'
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
         # raise ExitCmdException()
-        sys.exit(1)
+        # sys.exit(1)
+        return True
 
     def do_EOF(self, arg):
         """Exit with Ctrl-D"""
+        print()
         return True
 
 
